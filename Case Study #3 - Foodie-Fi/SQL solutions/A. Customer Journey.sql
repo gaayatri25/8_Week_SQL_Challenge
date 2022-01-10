72694df57f4e045f5b1420fa036b3d362090c063
gayatri <49824841+gayatriayu@users.noreply.github.com>
2022-01-10 23:25:08 +0700

    Add files via upload

---
+++ Case Study #3 - Foodie-Fi/SQL solutions/A. Customer Journey.sql
@@ -0,0 +1,24 @@
+SELECT s.customer_id, s.plan_id, p.plan_name, s.start_date
+FROM subscriptions s
+JOIN plans p
+ON s.plan_id = p.plan_id
+WHERE s.customer_id IN (1,2,11,13,15,16,18,19)
+
+SELECT s.customer_id, s.plan_id, p.plan_name, s.start_date
+FROM subscriptions s
+JOIN plans p
+ON s.plan_id = p.plan_id
+WHERE s.customer_id IN (1)
+
+SELECT s.customer_id, s.plan_id, p.plan_name, s.start_date
+FROM subscriptions s
+JOIN plans p
+ON s.plan_id = p.plan_id
+WHERE s.customer_id IN (11)
+
+SELECT s.customer_id, s.plan_id, p.plan_name, s.start_date
+FROM subscriptions s
+JOIN plans p
+ON s.plan_id = p.plan_id
+WHERE s.customer_id IN (16)
+
